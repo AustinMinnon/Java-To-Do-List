9f45232398e9f598959b3ad77b7fdbfb4c41373d
Abby <arolling@gmail.com>
2016-02-28 12:24:39 -0800

    Add join table interaction as per video

--- src/main/java/Category.java
+++ src/main/java/Category.java
@@ -1,5 +1,6 @@
 import java.util.List;
 import org.sql2o.*;
+import java.util.ArrayList;
 
 public class Category {
   private int id;
@@ -51,6 +52,50 @@ public class Category {
         .addParameter("id", id)
         .executeAndFetchFirst(Category.class);
       return Category;
+    }
+  }
+
+  public void addTask(Task task) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO categories_tasks (category_id, task_id) VALUES (:category_id, :task_id)";
+      con.createQuery(sql)
+        .addParameter("category_id", this.getId())
+        .addParameter("task_id", task.getId())
+        .executeUpdate();
+    }
+  }
+
+  public ArrayList<Task> getTasks() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT task_id FROM categories_tasks WHERE category_id = :category_id";
+      List<Integer> taskIds = con.createQuery(sql)
+        .addParameter("category_id", this.getId())
+        .executeAndFetch(Integer.class);
+
+      ArrayList<Task> tasks = new ArrayList<Task>();
+
+      for (Integer taskId : taskIds) {
+        String taskQuery = "SELECT * FROM tasks WHERE id = :taskId";
+        Task task = con.createQuery(taskQuery)
+          .addParameter("taskId", taskId)
+          .executeAndFetchFirst(Task.class);
+        tasks.add(task);
+      }
+    return tasks;
+    }
+  }
+
+  public void delete() {
+    try (Connection con = DB.sql2o.open()) {
+      String deleteQuery = "DELETE FROM categories WHERE id = :id;";
+      con.createQuery(deleteQuery)
+        .addParameter("id", id)
+        .executeUpdate();
+
+      String joinDeleteQuery = "DELETE FROM categories_tasks WHERE category_id = :categoryId";
+      con.createQuery(joinDeleteQuery)
+        .addParameter("categoryId", this.getId())
+        .executeUpdate();
     }
   }
 }
